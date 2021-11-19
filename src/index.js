016c276b51ce70e5dcf29bec8e34d2b29601fcff
Aaron Minnick <abminnick@gmail.com>
2021-11-19 14:27:13 -0800

    add styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 import ExchangeService from "./exchangeService";
 
 async function storeCodes() {

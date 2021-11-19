2c507f58c3366deb3541caa2b7a5843b93754dbf
Aaron Minnick <abminnick@gmail.com>
2021-11-19 11:51:47 -0800

    add js to correctly display conversion upon form submit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ExchangeService from "./exchangeService";
 
 async function storeCodes() {
   if (!sessionStorage.getItem('codes')) {
-    await ExchangeService.getCodes().then((response) => {
-      if (response.supported_codes) {
-        sessionStorage.setItem('codes', JSON.stringify(response.supported_codes));
-      } else {
-        $('#error-display').html(`<p>Error: ${response.message}</p>`);
-      }
-    });
+    await ExchangeService.getCodes()
+      .then((response) => {
+        if (response.supported_codes) {
+          sessionStorage.setItem('codes', JSON.stringify(response.supported_codes));
+        } else {
+          $('#error-display').html(`<p>Error: ${response.message}</p>`);
+        }
+      });
   }
 }
 
@@ -25,6 +26,32 @@ function populateSelects() {
   }
 }
 
-storeCodes().then(() => {
-  populateSelects();
+function displayExchange(amtFrom, currFrom, amtTo, currTo, rate) {
+  $('#exchange-display').html(`
+    <p>${amtFrom} ${currFrom} is equal to ${amtTo} ${currTo}.</p>
+    <p>Exchange rate: ${rate}</p>
+    `);
+}
+
+storeCodes()
+  .then(() => {
+    populateSelects();
+  });
+
+$('#exchange-form').on('submit', (event) => {
+  event.preventDefault();
+  let amtFrom = $('#amount').val();
+  let currFrom = $('#curr-from-select').val();
+  let currTo = $('#curr-to-select').val();
+
+  ExchangeService.getRate(currFrom, currTo, amtFrom)
+    .then((response) => {
+      if (response.conversion_result) {
+        let amtTo = response.conversion_result;
+        let rate = response.conversion_rate;
+        displayExchange(amtFrom, currFrom, amtTo, currTo, rate);
+      } else {
+        $('#error-display').html(`<p>Error: ${response.message}</p>`);
+      }
+    });
 });
